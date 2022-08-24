a1619a2d375c008104680d835f542a06ba920196
dzmitrymialeshka <110839344+dzmitrymialeshka@users.noreply.github.com>
2022-08-24 12:01:43 +0200

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -7,7 +7,7 @@ def test_button_add_to_basket(browser):
     link="http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     time.sleep(30)
-    addToBasket = browser.find_element(By.CSS_SELECTOR, "#add_to_basket_form > button").is_enabled()
+    addToBasket = browser.find_elements(By.CSS_SELECTOR, "#add_to_basket_form > button")
     assert addToBasket, "No 'add to basket' button."
     
     
